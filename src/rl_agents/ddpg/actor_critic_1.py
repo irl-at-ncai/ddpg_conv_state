d2a6ab6f80f862aea40117edd56e843665b8ac44
saifullah3396 <Saifullah3396@gmail.com>
2020-01-24 09:36:35 +0500

    The inputs required to the model will now be defined inside the model and their preprocessing will be handled by a PreprocessHandler() class in each model. See actor_critic_1.py for details. The class takes 'network_inputs' object from network yaml config to map the inputs from environment to  desired network inputs such as robot_state = [position, velocity], image = front_cam, etc. The network itself only takes dictionary input_shapes on construction and dictionary inputs on each call() which replaces the tuples used before.

--- src/rl_agents/ddpg/actor_critic_1.py
+++ src/rl_agents/ddpg/actor_critic_1.py
@@ -4,56 +4,140 @@ Defines the base deep model that is similar between the actor and critic
 """
 
 import os
+from operator import itemgetter
 import yaml
 import numpy as np
-from collections import namedtuple
 import tensorflow as tf
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import BatchNormalization
-from tensorflow.keras.activations import relu
+from tensorflow.keras.activations import relu, tanh
 from tensorflow.keras.initializers import random_uniform
 from tensorflow.keras.regularizers import l2
 from rl_agents.common.utils import inherit_docs
+from rl_agents.common.state_preprocessors import ImagePreprocessor, \
+    StateConcatenator
 import rospy
+
+
+class PreprocessHandler():
+    """
+    Maps the state from environment to the output state required by
+    the model.
+
+    Parameters
+    ----------
+    input_shapes_env: dict
+        Map from env observation keys to their shapes
+    cfg: str
+        Config name of the model
+    """
+    def __init__(self, input_shapes_env, cfg='actor_critic_1'):
+        script_dir = os.path.dirname(os.path.realpath(__file__))
+        with open(script_dir + '/cfg/' + cfg + '.yaml') as config_file:
+            self.config = yaml.load(config_file, Loader=yaml.FullLoader)
+        self.network_inputs = self.config['network_inputs']
+
+        # update the shapes to what we want
+        self.input_shapes_env = input_shapes_env
+        self.input_shapes = {}
+        for target_key, keys in self.network_inputs.items():
+            shapes = [input_shapes_env[key] for key in keys]
+            self.input_shapes[target_key] = tuple(map(sum, zip(*shapes)))
+        self.setup_input_preprocessors()
+
+        rospy.loginfo("Network takes the following inputs:")
+        for idx, (key, value) in \
+                enumerate(self.input_shapes.items()):
+            rospy.loginfo("{}) {} ({}) (shape = {})".format(
+                idx, key, self.network_inputs[key], value))
+
+    def setup_input_preprocessors(self):
+        """
+        Sets up the preprocessors that convert/reshape inputs from
+        environment to the required shapes for the network.
+        """
+        self.preprocessors = {}
+        for key, value in self.network_inputs.items():
+            print(key)
+            # define a preprocess for robot state
+            if key == 'robot_state':
+                self.preprocessors[key] = \
+                    StateConcatenator(output_name=key)
+
+            # define a preprocessor for image input
+            if key == 'image':
+                self.preprocessors[key] = \
+                    ImagePreprocessor(
+                        input_shape=self.input_shapes_env[value[0]],
+                        output_shape=tuple(self.config['input_image_shape']))
+                self.input_shapes[key] = self.preprocessors[key].output_shape
+
+            # define a preprocessor for image depth input
+            if key == 'image_depth':
+                self.preprocessors[key] = \
+                    ImagePreprocessor(
+                        input_shape=self.input_shapes_env[value[0]],
+                        output_shape=tuple(
+                            self.config['input_depth_image_shape']))
+                self.input_shapes[key] = self.preprocessors[key].output_shape
+        if len(self.preprocessors) != len(self.network_inputs):
+            rospy.logwarn("Preprocessors not defined for all inputs.")
+
+    def process(self, state, sess):
+        """
+        Performs pre-processing operations on the state
+        """
+        # update the states to what we want in agent
+
+        processed = {}
+        for key, value in self.network_inputs.items():
+            processed[key] = \
+                self.preprocessors[key]. \
+                process(itemgetter(*value)(state), sess)
+            processed[key] = processed[key][np.newaxis, ...]
+        return processed
 
 
 class ActorCriticBase(tf.keras.Model):
     """
-    A critic model for DDPG algorithm used that takes
+    An actor-critic model for DDPG algorithm used that takes
     image, robot state, and action inputs to determine
     a Q-value for the actions.
 
     Parameters
     ----------
-    image_input_shape : tuple
-        Shape of the input image
-    robot_state_input_shape: tuple
-        Shape of the input robot state
-    actions_input_shape: tuple
-        Shape of the input actions
+    input_shapes: tuple
+        Shapes of the input image and robot state that this deep qn deals with.
+        Example input_shapes={
+                'image': (80, 60, 3),
+                'robot_state': (13,)}
     scope: str
         Tensorflow variable scope
     """
+
     # pylint: disable=too-many-instance-attributes
     def __init__(
             self,
-            image_input_shape=(80, 60, 3),
-            robot_state_input_shape=(13,),
+            input_shapes,
             scope='actor_critic_1',
             cfg='actor_critic_1'):
         with tf.name_scope(scope):
             super(ActorCriticBase, self).__init__()
 
-            if any(
-                    not isinstance(x, tuple)
-                    for x in [robot_state_input_shape, image_input_shape]):
-                rospy.logerr('Model input shapes must be a tuple.')
-
             script_dir = os.path.dirname(os.path.realpath(__file__))
             with open(script_dir + '/cfg/' + cfg + '.yaml') as config_file:
                 self.config = yaml.load(config_file, Loader=yaml.FullLoader)
+            self.input_shapes = input_shapes
+
+            if any(
+                    not isinstance(v, tuple)
+                    for k, v in self.input_shapes.items()):
+                rospy.logerr(
+                    'Model input shapes {} must be tuples.'
+                    .format(self.input_shapes))
+
             conv_layer_sizes = self.config['conv_layers']['sizes']
             conv_kernels = self.config['conv_layers']['kernels']
             dense_layer_sizes = self.config['dense_layers']['sizes']
@@ -63,7 +147,7 @@ class ActorCriticBase(tf.keras.Model):
                 self.make_conv2d_layer(
                     conv_layer_sizes['c1'],
                     conv_kernels['c1'],
-                    input_shape=image_input_shape)
+                    input_shape=self.input_shapes['image'])
             self.conv2 = \
                 self.make_conv2d_layer(
                     conv_layer_sizes['c2'], conv_kernels['c2'])
@@ -75,12 +159,12 @@ class ActorCriticBase(tf.keras.Model):
             # state input to dense layer connection
             self.dense4_state = \
                 self.make_dense_layer(
-                    dense_layer_sizes['d4'], robot_state_input_shape)
+                    dense_layer_sizes['d4'], self.input_shapes['robot_state'])
 
     # pylint: disable=arguments-differ
     def call(self, inputs):
         # layer 1 with image input
-        c_1 = self.conv1(inputs.image)
+        c_1 = self.conv1(inputs['image'])
         c_1 = BatchNormalization()(c_1)
         c_1 = relu(c_1)
 
@@ -100,7 +184,7 @@ class ActorCriticBase(tf.keras.Model):
         d_4_image = BatchNormalization()(d_4_image)
 
         # connect state input to d4_image layer
-        d_4_state = self.dense4_state(inputs.robot_state)
+        d_4_state = self.dense4_state(inputs['robot_state'])
         d_4_state = BatchNormalization()(d_4_state)
 
         # add the outputs of two layers and activate
@@ -173,19 +257,31 @@ class ActorCriticBase(tf.keras.Model):
 
 
 class CriticModel(ActorCriticBase):
-    @inherit_docs
+    """
+    A critic model for DDPG algorithm.
+
+    Parameters
+    ----------
+    input_shapes: tuple
+        Shapes of the input image and robot state that this deep qn deals with.
+        Example input_shapes={
+                'image': (80, 60, 3),
+                'robot_state': (13,),
+                'actions': (4,)}
+    scope: str
+        Tensorflow variable scope
+    cfg: str
+        Config file of the model
+    """
     # pylint: disable=too-many-instance-attributes
     def __init__(
             self,
-            image_input_shape=(80, 60, 3),
-            robot_state_input_shape=(13,),
-            actions_input_shape=(4,),
+            input_shapes,
             scope='critic_model_1',
             cfg='actor_critic_1'):
         with tf.name_scope(scope):
             super(CriticModel, self).__init__(
-                image_input_shape,
-                robot_state_input_shape,
+                input_shapes,
                 scope,
                 cfg)
 
@@ -198,7 +294,8 @@ class CriticModel(ActorCriticBase):
             # final dense layer for actions input
             self.dense5_actions = \
                 self.make_dense_layer(
-                    dense_layer_sizes['d5'], input_shape=actions_input_shape)
+                    dense_layer_sizes['d5'],
+                    input_shape=input_shapes['actions'])
             # returns the q-value for the action taken
             self.dense6 = \
                 Dense(
@@ -215,11 +312,6 @@ class CriticModel(ActorCriticBase):
     def call(self, inputs):
         """
         Performs the forward pass of the critic network
-
-        Parameters
-        ----------
-        inputs: CriticInputs
-            Inputs to the network
         """
 
         d_4 = super(CriticModel, self).call(inputs)
@@ -230,7 +322,7 @@ class CriticModel(ActorCriticBase):
 
         # get action input from actor and create another layer in parallel to
         # d5_features
-        d_5_actions = self.dense5_actions(inputs.actions)
+        d_5_actions = self.dense5_actions(inputs['actions'])
         d_5_actions = relu(tf.add(d_5_features, d_5_actions))
 
         # layer 6
@@ -239,19 +331,32 @@ class CriticModel(ActorCriticBase):
 
 @inherit_docs
 class ActorModel(ActorCriticBase):
+    """
+    An actor model for DDPG algorithm.
+
+    Parameters
+    ----------
+    input_shapes: tuple
+        Shapes of the input image and robot state that this deep qn deals with.
+        Example input_shapes={
+                'image': (80, 60, 3),
+                'robot_state': (13,)}
+    scope: str
+        Tensorflow variable scope
+    cfg: str
+        Config file of the model
+    """
     # pylint: disable=too-many-instance-attributes
     def __init__(
             self,
             action_bound,
-            image_input_shape=(80, 60, 3),
-            robot_state_input_shape=(13,),
+            input_shapes,
             actions_output_shape=(4,),
             scope='actor_model_1',
             cfg='actor_critic_1'):
         with tf.name_scope(scope):
             super(ActorModel, self).__init__(
-                image_input_shape,
-                robot_state_input_shape,
+                input_shapes,
                 scope,
                 cfg)
 
@@ -281,11 +386,6 @@ class ActorModel(ActorCriticBase):
     def call(self, inputs):
         """
         Performs the forward pass of the actor network
-
-        Parameters
-        ----------
-        inputs: ActorInputs
-            Inputs to the network
         """
 
         d_4 = super(ActorModel, self).call(inputs)
@@ -295,6 +395,7 @@ class ActorModel(ActorCriticBase):
         d_5_features = BatchNormalization()(d_5_features)
         d_5_features = relu(d_5_features)
 
+        d_6 = self.dense6(d_5_features)
+        d_6 = tanh(d_6)
         # layer 6
-        return tf.math.multiply(self.dense6(d_5_features), self.action_bound)
-
+        return tf.math.multiply(d_6, self.action_bound)
